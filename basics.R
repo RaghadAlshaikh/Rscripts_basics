3351ac8fca14e7c3aa9adcbece8573678f0df54a
Raghad Alshaikh <raghad.alshaikh@outlook.com>
2020-12-07 05:51:44 +0300

    Create basics.R

---
+++ basics.R
@@ -0,0 +1,21 @@
+#Date: 7 December, 2020
+
+#----Packages Installation start----
+
+#Installing a single package
+#install.packages("dslabs") 
+
+#Installing multiple packages at once
+install.packages(c("tidyverse", "dslabs")）
+
+#List all the installed packages
+installed.packages()
+
+#Load an installed package
+library(dslabs)
+
+#----Packages Installation ends----
+
+
+#All the credits are for this great program that I signed up for: 
+# https://www.edx.org/professional-certificate/harvardx-data-science
