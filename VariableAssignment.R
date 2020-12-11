663998b81935c8cecd66cd13020688e101afb638
Raghad Alshaikh <raghad.alshaikh@outlook.com>
2020-12-11 03:45:25 +0300

    Create VariableAssignment.R

---
+++ VariableAssignment.R
@@ -0,0 +1,14 @@
+# Assign the value 42 to x
+x <- 42
+# Assign the value 30 to y
+y <- 30
+
+# Print out the value of the variable x
+x
+y
+
+# Add these two variables together
+x + y
+
+# Create the variable z
+z = x + y
