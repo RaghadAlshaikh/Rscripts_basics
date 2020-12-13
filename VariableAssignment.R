a9eaeab5955188f494e68b09e6bebfa4a61dab75
Raghad Alshaikh <raghad.alshaikh@outlook.com>
2020-12-13 23:53:49 +0300

    Update VariableAssignment.R

--- VariableAssignment.R
+++ VariableAssignment.R
@@ -12,3 +12,17 @@ x + y
 
 # Create the variable z
 z = x + y
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
